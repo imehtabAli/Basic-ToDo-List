ac390df7ba1dc9b67dcbb1628fadb4820a28159b
imehtabAli <mehtabg0709@gmail.com>
2024-04-29 23:27:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+const addButton = document.getElementById("addDataButton");
+const inputData = document.getElementById("inputData");
+const idList = document.querySelector("#idList");
+
+function panel(){
+
+    if (inputData.value.trim() === '') {
+        return;
+    }
+
+    const toDoData = document.createElement("div");
+    toDoData.className = "toDoData";
+
+    const textData = document.createElement("p");
+    textData.className = "textData";
+    textData.textContent = inputData.value;
+    
+    const remove = document.createElement("button");
+    remove.id = "remove";
+    remove.innerText = "Remove"
+    remove.addEventListener("click", function(){
+        idList.removeChild(toDoData)
+    })
+    
+    idList.classList.add('idListStyle');
+    toDoData.appendChild(textData);
+    toDoData.appendChild(remove);
+    idList.appendChild(toDoData);
+
+    inputData.value = "";
+
+}
+
+addButton.addEventListener("click", panel);
+
+inputData.addEventListener("keydown", function(event) {
+    if (event.keyCode === 13) {
+        panel();
+    }
+});
